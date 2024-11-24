3dda3966097fd463ffe373ae080880bc9bece514
Jon Bennett <147949701+JAB15NCE@users.noreply.github.com>
2024-11-24 15:13:58 -0800

    Update and rename New Text Document.txt to spinword.py

---
+++ Python/Stop gninnipS My sdroW!/spinword.py
@@ -0,0 +1,7 @@
+def spin_words(sentence):
+    words = sentence.split()
+    result = [
+        word[::-1] if len(word) >= 5 else word
+        for word in words               
+    ]
+    return ' '.join(result)
